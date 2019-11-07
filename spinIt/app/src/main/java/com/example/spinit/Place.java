f805c2dc621ac0c88209685c0c3d809be3bbb29e
Brian Du <briantdu777@gmail.com>
2019-11-07 00:09:00 -0800

    updated places

--- spinIt/app/src/main/java/com/example/spinit/Place.java
+++ spinIt/app/src/main/java/com/example/spinit/Place.java
@@ -1,4 +1,25 @@
 package com.example.spinit;
 
 public class Place {
+    private double longitude;
+    private double latitude;
+    private String url;
+    Place(double longitude, double latitude, String url)
+    {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.url = url;
+    }
+    double getLongitude()
+    {
+        return this.longitude;
+    }
+    double getLatitude()
+    {
+        return this.longitude;
+    }
+    String URL()
+    {
+        return this.url;
+    }
 }
