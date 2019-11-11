39168964948346196d13596b8ac1c58da7a5a7e0
Brian Du <briantdu777@gmail.com>
2019-11-10 20:21:29 -0800

    added some test cases

--- spinIt/app/src/main/java/com/example/spinit/Person.java
+++ spinIt/app/src/main/java/com/example/spinit/Person.java
@@ -7,52 +7,61 @@ public class Person {
     private ArrayList<String> listOfStatus;
     private Set<String> listOfFriends;
     private String username;
+    private SpunPlaces spunPlaces;
+
     //private Restrict dietaryList;
 
-    Person(ArrayList<String> listOfStatus, Set<String> listOfFriends, String username){
+    Person(ArrayList<String> listOfStatus, Set<String> listOfFriends, String username,
+           SpunPlaces spunPlaces){
         this.listOfFriends = listOfFriends;
         this.listOfStatus = listOfStatus;
         this.username = username;
+        this.spunPlaces = spunPlaces;
         //this.dietaryList = dietaryList
     }
-    void updateStatus(String status){
+    boolean updateStatus(String status){
         //Here we will add the status into our arrayList as another entry
         boolean checker = this.listOfStatus.add(status);
         if(!checker) {
             System.out.printf("Status cannot be updated");
+            return false;
         }
         //****A database call will be needed at the end of this function****
+        return true;
     }
 
-    void addFriend(String username){
+    boolean addFriend(String username){
         boolean checker = true;
         //Do a database check to see if the username is a valid
         //one and set the checker to true if it exists
 
         if(!checker) {
             System.out.printf("User Cannot be found, no friend added");
-            return;
+            return false;
         }
         checker = this.listOfFriends.add(username);
         if(!checker) {
             System.out.printf("User already is a friend");
-            return;
+            return false;
         }
         //****A database call will be needed at the end of this function****
+        return true;
     }
-    void setUsername(String username){
+    boolean setUsername(String username){
         boolean checker = true;
         //Do a database check to see if that username is used
         //if it is, then go ahead and set checker to false
         if(!checker) {
             System.out.printf("Username is taken");
-            return;
+            return false;
         }
         this.username = username;
         //****A database call will be needed at the end of this function****
+        return true;
     }
     ArrayList<String> getListOfStatus(){return this.listOfStatus;}
     Set<String> getListOfFriends(){return this.listOfFriends;}
     String getUsername(){return this.username;}
+    SpunPlaces getSpunPlaces(){return this.spunPlaces;}
 
 }
--- spinIt/app/src/main/java/com/example/spinit/Place.java
+++ spinIt/app/src/main/java/com/example/spinit/Place.java
@@ -10,6 +10,13 @@ public class Place {
         this.latitude = latitude;
         this.url = url;
     }
+    public boolean equals(Object o) {
+        Place other = (Place) o;
+        if(this.longitude == other.getLongitude() && this.latitude == other.getLatitude() && this.url.equals(other.getURL()))
+            return true;
+        else
+            return false;
+    }
     double getLongitude(){ return this.longitude;}
     double getLatitude(){ return this.latitude; }
     String getURL(){ return this.url; }
